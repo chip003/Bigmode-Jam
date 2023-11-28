7bc9071e4613cf2e034e2b57d52f009b1e332a95
chip03 <111972339+chip003@users.noreply.github.com>
2023-11-27 17:23:02 -0700

    Add outer corner tiles and camera bounds

--- Camera2D.gd
+++ Camera2D.gd
@@ -4,12 +4,19 @@ var mouseStartPos
 var startPos
 
 var speedFactor = 1000
+var speed = speedFactor
+
+var minBounds = Vector2(-1000,-1000)
+var maxBounds = Vector2(1000,1000)
 
 func _process(delta):
 	if Input.is_action_pressed("shift"):
-		position += Input.get_vector("left","right","up","down")*(speedFactor*2)*delta/zoom.x
+		speed = speedFactor*2
 	else:
-		position += Input.get_vector("left","right","up","down")*speedFactor*delta/zoom.x
+		speed = speedFactor
+
+	position += Input.get_vector("left","right","up","down")*speedFactor*delta/zoom.x
+	position = position.clamp(minBounds,maxBounds)
 	
 	if Input.is_action_just_pressed("zoomin"):
 		if zoom.x < 5:
